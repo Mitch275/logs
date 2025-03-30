6a7dad66f34e305d047fc35cfbcf5696fae06cd4
mitch <99443939+Mitch275@users.noreply.github.com>
2025-03-30 16:17:07 -0400

    Update latest.py

--- latest.py
+++ latest.py
@@ -1,5 +1,26 @@
 # updated to 1.2
 
-import PySimpleGUI as sg
+import sys
 
-sg.Window(title="Hello World", layout=[[]], margins=(100, 50)).read()
+from PyQt5.QtCore import QSize, Qt
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+
+
+# Subclass QMainWindow to customize your application's main window
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("My App")
+        button = QPushButton("Press Me!")
+
+        # Set the central widget of the Window.
+        self.setCentralWidget(button)
+
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec()
